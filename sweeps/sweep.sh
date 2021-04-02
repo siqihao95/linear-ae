83106c4132136cd3282cbe2cf734c82ab5c66d92
Sheldon <hscxwmr@gmail.com>
2021-04-02 17:42:25 -0400

    trying..

--- sweeps/sweep.sh
+++ sweeps/sweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd /h/huang/git_code/lae/linear-ae
 export PYTHONPATH="~/git_code/lae/linear-ae"
 wandb login f75b0173d54548337e4ca0962a4411d3e5292598
 wandb agent sheldonhuang/linear-ae-neurips/tu2nswpm
