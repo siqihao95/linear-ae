34674b764571acbcbe28ecaad640f3226a1f8975
Sheldon <hscxwmr@gmail.com>
2021-04-05 16:53:44 -0400

    sweep id

--- sweeps/s0405.sh
+++ sweeps/s0405.sh
@@ -3,4 +3,4 @@
 # cd /h/huang/git_code/lae/linear-ae
 export PYTHONPATH="/h/huang/git_code/lae/linear-ae"
 wandb login f75b0173d54548337e4ca0962a4411d3e5292598
-wandb agent sheldonhuang/linear-ae-rms/9jo7orcy
+wandb agent sheldonhuang/linear-ae-rms/2micb4r5
