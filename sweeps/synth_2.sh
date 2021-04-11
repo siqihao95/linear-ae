4d156f71d6d52d971bc14c7c5b0eeb8900287d55
Sheldon <hscxwmr@gmail.com>
2021-04-11 15:37:55 -0400

    project id

--- sweeps/synth_2.sh
+++ sweeps/synth_2.sh
@@ -3,4 +3,4 @@
 # cd /h/huang/git_code/lae/linear-ae
 export PYTHONPATH="/h/huang/git_code/lae/linear-ae"
 wandb login f75b0173d54548337e4ca0962a4411d3e5292598
-wandb agent sheldonhuang/lae-rms-synth/v2wy7nfr
+wandb agent sheldonhuang/lae-rms-synth/o6e1u7ld
