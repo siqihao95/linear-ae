c5d33b71db8786362cea260267cfde070762946e
Sheldon <hscxwmr@gmail.com>
2021-04-10 22:17:51 -0400

    sbatch

---
+++ sweeps/synth.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# cd /h/huang/git_code/lae/linear-ae
+export PYTHONPATH="/h/huang/git_code/lae/linear-ae"
+wandb login f75b0173d54548337e4ca0962a4411d3e5292598
+wandb agent sheldonhuang/lae-rms-naive/v2wy7nfr
